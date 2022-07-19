9426186ace7c9735e3c1f629796e56c0e066370a
G_P_S <gustavo-pereira23@protonmail.com>
2022-07-18 23:54:32 -0300

    Commit alpha

--- src/components/contato/contato.jsx
+++ src/components/contato/contato.jsx
@@ -3,8 +3,22 @@ import './contato.css'
 import {AiOutlineMail} from 'react-icons/ai'
 import {BsLinkedin} from 'react-icons/bs'
 import {AiOutlineWhatsApp} from 'react-icons/ai'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contato = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_40d8eof', 'template_e5hmsif', form.current, 'zWXeDYbdoLWZgHTin')
+  
+    e.target.reset();
+
+    alert("Mensagem enviada!");
+  };
+
   return (
     <section id='contato'>
       <h2>Contato</h2>
@@ -32,7 +46,7 @@ const Contato = () => {
             <a href="https://api.whatsapp.com/send?phone=5551995881983" target="_blank">Envie uma mensagem</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Nome completo' required/>
           <input type="email" name='email' placeholder='Email' required/>
           <textarea name="message" rows="10" placeholder='Escreva uma mensagem' required></textarea>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {SiVercel} from 'react-icons/si'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Gustavo Pereira dos Santos</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Início</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Conhecimentos</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+
+      <div className='footer_Socials'>
+          <a href="https://www.linkedin.com/in/gustavo-pereira-dos-santos/" target="_blank"><BsLinkedin /></a>
+          <a href="https://github.com/GPS33" target="_blank"><BsGithub /></a>
+          <a href="https://vercel.com/gps33" target="_blank"><SiVercel /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; 2022, Desenvolvido por Gustavo Pereira dos Santos. </small>
+      </div>
+    </footer>
   )
 }
 
