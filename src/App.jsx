9d23343ff5e4ed33ca435d73b97a7601ff7422a5
G_P_S <gustavo-pereira23@protonmail.com>
2022-07-19 23:16:17 -0300

    --2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
-import Sobre from './components/sobre/Sobre'
+
 import Conhecimentos from './components/conhecimentos/Conhecimentos'
 import Portfolio from './components/portfolio/Portfolio'
 import Contato from './components/contato/Contato'
